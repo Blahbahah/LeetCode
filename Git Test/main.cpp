6e229d2e27eb3eccefe803facb63cb9f66876f65
Tyler Schneider <Blahbahah@Emilys-MacBook-Air.local>
2019-07-08 13:02:28 -0400

    Initial Commit

---
+++ Git Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Git Test
+//
+//  Created by Tyler Schneider on 7/8/19.
+//  Copyright © 2019 Tyler Schneider. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
